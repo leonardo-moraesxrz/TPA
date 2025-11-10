8b15ed817e58180c7b303de94be137692183e5b5
leonardo-moraesxrz <lleonardo.moraes2009@gmail.com>
2025-11-10 09:21:23 -0300

    Update TabuadaVetor.java

--- TreinoDeVetores/TabuadaVetor.java
+++ TreinoDeVetores/TabuadaVetor.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 
 public class TabuadaVetor {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner ler = new Scanner(System.in);
         int[] A = new int[5];
 
-        // Leitura dos 5 números do vetor
         System.out.println("Digite 5 números inteiros:");
         for (int i = 0; i < 5; i++) {
             System.out.print("A[" + i + "]: ");
-            A[i] = sc.nextInt();
+            A[i] = ler.nextInt();
         }
 
         System.out.println("\n--- TABUADAS ---");
 
-        // Imprime a tabuada de cada elemento
         for (int i = 0; i < 5; i++) {
             System.out.println("\nTabuada do " + A[i] + ":");
             for (int j = 1; j <= 10; j++) {
@@ -22,6 +20,6 @@ public class TabuadaVetor {
             }
         }
 
-        sc.close();
     }
 }
+
